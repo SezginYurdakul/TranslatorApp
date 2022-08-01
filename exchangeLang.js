c52ff7bc63926d48a9f5781251d1460ad661ba51
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-08-01 16:36:05 +0200

    exchange lang funtion and some css styling changes are made

--- exchangeLang.js
+++ exchangeLang.js
@@ -1,10 +1,9 @@
-const selectTag = document.querySelectorAll("select");
-const fromText = document.querySelector(".from-text");
-const toText = document.querySelector(".to-text");
-
 export function exchangeLanguages() {
+  const selectTag = document.querySelectorAll("select");
+  const fromText = document.querySelector(".from-text");
+  const toText = document.querySelector(".to-text");
   let tempText = fromText.value;
-  tempLang = selectTag[0].value;
+  const tempLang = selectTag[0].value;
   fromText.value = toText.value;
   toText.value = tempText;
   selectTag[0].value = selectTag[1].value;
